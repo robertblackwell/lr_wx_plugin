9728b31b4b6ad2395378d05ce417bf690bf9d8ae
Robert BLACKWELL <robertblackwell@Roberts-MacBook-Pro.local>
2025-08-26 19:54:18 +1000

    starting to build te set of plugin specfic parameters and neew Constants.lua file - still works

---
+++ ftp_upload.lrdevplugin/Constants.lua
@@ -0,0 +1,4 @@
+Constants = {}
+Constants.ImageTypes = {large = "large_images", thumbnails = "thumbnails"}
+Constants.AlbumTypes = {journal = "journal", photo = "photo"}
+return Constants
--- ftp_upload.lrdevplugin/ExportServiceProvider.lua
+++ ftp_upload.lrdevplugin/ExportServiceProvider.lua
@@ -1,19 +1,22 @@
 require "ExportDialogSections"
 require "ExportTask"
 require "UpdateLrExportSettings"
+require "Constants"
 
 return {
-	
 	hideSections = { 'video', 'exportLocation', 'fileSettings', 'watermarking', 'metadata', 'outputSharpening', 'fileNaming', "imageSettings" },
-
 	allowFileFormats = {'JPEG'}, -- nil equates to all available formats
 	canExportVideo = false,
 	allowColorSpaces = {'AdobeRGB'}, -- nil equates to all color spaces
-
 	exportPresetFields = {
 		{ key = 'WX_exportPrefix', default = '/Users/robertblackwell/LrPlugins/TestExports' },
 		{ key = 'WX_exportFolder', default = 'photos2'},
-		{ key = 'WX_synopsis', default = 'Export photos for whiteacorn.com website'},
+		{ key = 'WX_synopsis', default = 'Export photo albums for whiteacorn.com website'},
+		{ key = 'WX_albumType', default = Constants.AlbumTypes.journal },
+		{ key = 'WX_imageType', default = Constants.ImageTypes.large},
+		{ key = 'WX_slug', default = nil},
+		{ key = 'WX_journal_album_name', default = nil},
+
 		{ key = 'putInSubfolder', default = false },
 		{ key = 'path', default = 'photos' },
 		{ key = "ftpPreset", default = nil },
@@ -23,5 +26,4 @@ return {
 	sectionsForTopOfDialog = ExportDialogSections.sectionsForTopOfDialog,
 	updateExportSettings = UpdateLrExportSettings.update,
 	processRenderedPhotos = ExportTask.processRenderedPhotos,
-	
 }
